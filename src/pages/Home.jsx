4e053d45e32b73f7e8f0e29b4af7effa686e69d6
Viktor.A <viktor.antonyshyn@gmail.com>
2023-09-08 13:48:12 +0200

    Fixed css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,19 @@
 import ShowProject from "../components/ShowProject";
 import { NavLink } from "react-router-dom";
-
+import './Home.css'
 
 const Home = () => {
     return (
         <>
-            <NavLink to = "/create"><button className="buttons">Subir Proyesto</button></NavLink>
-            <h2>Lista de Proyectos:</h2>
-            
-            <div>
-                <ShowProject />
+            <div className="page">
+                <NavLink to="/create"><button className="buttons">Subir Proyesto</button></NavLink>
+                <h2>Lista de Proyectos:</h2>
+
+                <div>
+                    <ShowProject />
+                </div>
             </div>
+
         </>
     )
 }
