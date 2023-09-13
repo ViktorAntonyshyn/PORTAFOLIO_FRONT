04b00b2ce55dc73408f83becaad534efef954dd8
Viktor.A <viktor.antonyshyn@gmail.com>
2023-09-13 10:17:59 +0200

    Added component Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+
+const Header = () => {
+
+    return (
+        <>
+
+            <div className="container-project" >
+                <p> Header para mi pagina</p>
+            </div>
+
+        </>
+
+    );
+}
+
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import ShowProject from "../components/ShowProject";
 import { NavLink } from "react-router-dom";
 import './Home.css'
+import Header from "../components/Header";
 
 const Home = () => {
     return (
         <>
+            <div>
+                <Header />
+            </div>
             <div className="page">
                 <NavLink to="/create"><button className="buttons">Subir Proyesto</button></NavLink>
                 <h2>Lista de Proyectos:</h2>
