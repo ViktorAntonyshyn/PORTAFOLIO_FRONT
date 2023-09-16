c51b056abbe063b38e6d6ba43db0a2356e189da7
Viktor.A <viktor.antonyshyn@gmail.com>
2023-09-16 16:54:17 +0200

    Contacts WORKS!!

--- src/components/Atomos/Cont.jsx
+++ src/components/Atomos/Cont.jsx
@@ -2,8 +2,39 @@ import './Cont.css'
 
 
 const Cont = () => {
+    return (
+        <><div className='container-contact'>
+            <h3> Me contacts:</h3>
+            <p>+34 624036489 /
+                Madrid </p>
+            <h3>WatsApp:</h3>
+            <a href="https://wa.me/qr/2Y7CB33BIZOYP1" target="_blank" rel="noopener noreferrer">
+                <p>https://wa.me/qr/2Y7CB33BIZOYP1</p>
+            </a>
 
-    <h3>Contats:</h3>
+            <h3>Telegram:</h3>
+            <a href="https://www.t.me/BIKTOP_A" target="_blank" rel="noopener noreferrer">
+                <p>https://www.t.me/BIKTOP_A</p>
+            </a>
+
+            <h3>LinkedIn:</h3>
+            <a href="https://www.linkedin.com/in/viktor-antonyshyn/" target="_blank" rel="noopener noreferrer">
+                <p>https://www.linkedin.com/in/viktor-antonyshyn/</p>
+            </a>
+
+            <h3>GitHub:</h3>
+            <a href="https://github.com/ViktorAntonyshyn" target="_blank" rel="noopener noreferrer">
+                <p>https://github.com/ViktorAntonyshyn</p>
+            </a>
+
+        </div>
+
+        </>
+
+
+    )
+
+
 
 };
 
--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,3 +1,4 @@
+import Cont from "../components/Atomos/Cont";
 import Footer from "../components/Atomos/Footer";
 import Languages from "../components/Atomos/Languages";
 import Navbar1 from "../components/Atomos/Navbar1";
@@ -8,8 +9,8 @@ const Contacts = () => {
       <><div>
         <Navbar1/>
       </div>
-        <div className='lang'>
-          <Languages />
+        <div>
+          <Cont />
         </div>
         <div>
             <Footer/>
