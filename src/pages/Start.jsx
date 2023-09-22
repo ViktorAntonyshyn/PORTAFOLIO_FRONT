5fc03213eb4a2ad705767f10f3e705a046815ef2
Viktor.A <viktor.antonyshyn@gmail.com>
2023-09-22 10:36:48 +0200

    Backed all chenges for test vercel

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -8,15 +8,8 @@ const Start = () => {
         <>
             <div>
                 <Navbar1 />
-                <h3>Test</h3>
-                <h3>Test</h3>
-                <h3>Test</h3>
-                <h3>Test</h3>
-                <h3>Test</h3>
             </div>
             <div>
-            <h3>Test1</h3> <h3>Test1</h3> <h3>Test1</h3> <h3>Test1</h3>
-            <img className="pic-start" src="/src/pics/background.png" alt="Alternative Text" />
                 <a href="/about">
                     <img className="pic-start" src="/src/pics/back1.png" alt="Alternative Text" />
                 </a>
