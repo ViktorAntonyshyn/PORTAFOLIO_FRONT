3b759a38a4ff7edee9fe4cf1ce5fc1c5eb03d86c
Viktor.A <viktor.antonyshyn@gmail.com>
2024-07-27 15:25:18 +0200

    Change Languages

--- src/components/Atomos/Languages.jsx
+++ src/components/Atomos/Languages.jsx
@@ -11,30 +11,27 @@ function Languages() {
                 <div className='lang'>
                     {/* <h5>Languages</h5> */}
                 </div>
-
-
-                <div className="progress" role="progressbar" aria-label="Basic example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
-                    <span>3.  </span>
-                    <div className="progress-bar" style={{ width: '60%' }}>Polish</div>
-                </div> <br />
-                <div className="progress" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                    <span>4.  </span>
-                    <div className="progress-bar" style={{ width: '90%' }}>Russian</div>
-                </div> <br />
                 <div className="progress" role="progressbar" aria-label="Basic example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
                     <span>5.  </span>
                     <div className="progress-bar" style={{ width: '99%' }}>Ukraniano - nativo</div>
                 </div><br />
-                <div className="progress" role="progressbar" aria-label="Basic example" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
-                    <span>5.  </span>
-                    <div className="progress-bar" style={{ width: '27%' }}>English</div>
+                <div className="progress" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                    <span>4.  </span>
+                    <div className="progress-bar" style={{ width: '90%' }}>Russian</div>
+                </div> <br />
+                <div className="progress" role="progressbar" aria-label="Basic example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                    <span>3.  </span>
+                    <div className="progress-bar" style={{ width: '60%' }}>Polish</div>
                 </div> <br />
                 <div className="progress" role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
                     <span>2.  </span>
                     <div className="progress-bar" style={{ width: '40%' }}>Spanish</div>
                 </div> <br />
-                
-                </div>
+                <div className="progress" role="progressbar" aria-label="Basic example" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
+                    <span>5.  </span>
+                    <div className="progress-bar" style={{ width: '27%' }}>English</div>
+                </div> <br />
+            </div>
 
         </>
     );
