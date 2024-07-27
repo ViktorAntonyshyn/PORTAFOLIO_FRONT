bb6b557e10997947b26988ff50eff8480e34e8cb
Viktor.A <viktor.antonyshyn@gmail.com>
2024-07-27 15:26:04 +0200

    Change Languages

--- src/components/Atomos/Languages.jsx
+++ src/components/Atomos/Languages.jsx
@@ -12,11 +12,11 @@ function Languages() {
                     {/* <h5>Languages</h5> */}
                 </div>
                 <div className="progress" role="progressbar" aria-label="Basic example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
-                    <span>5.  </span>
+                    <span>1.  </span>
                     <div className="progress-bar" style={{ width: '99%' }}>Ukraniano - nativo</div>
                 </div><br />
                 <div className="progress" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                    <span>4.  </span>
+                    <span>2.  </span>
                     <div className="progress-bar" style={{ width: '90%' }}>Russian</div>
                 </div> <br />
                 <div className="progress" role="progressbar" aria-label="Basic example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
@@ -24,7 +24,7 @@ function Languages() {
                     <div className="progress-bar" style={{ width: '60%' }}>Polish</div>
                 </div> <br />
                 <div className="progress" role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-                    <span>2.  </span>
+                    <span>4.  </span>
                     <div className="progress-bar" style={{ width: '40%' }}>Spanish</div>
                 </div> <br />
                 <div className="progress" role="progressbar" aria-label="Basic example" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
