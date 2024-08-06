c60941de92ddded9fbe5a044172b846e49a4a873
Viktor.A <viktor.antonyshyn@gmail.com>
2024-08-06 21:15:55 +0200

    Modifided CV

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -25,7 +25,7 @@ export const router = createBrowserRouter ([
         element: <Home/>
     },
     {
-        path: "/projects/manager",
+        path: "/",
         element: <ProjectManager/>
     },
     {
